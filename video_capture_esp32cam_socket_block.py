22958b2aeac02baa62d457c14423b33d736370ca
conner <conner.jeong@gmail.com>
2020-02-26 00:02:14 +0900

    simplifed

--- video_capture_esp32cam_socket_block.py
+++ video_capture_esp32cam_socket_block.py
@@ -1,37 +1,18 @@
 import cv2 as cv
 import numpy as np
-from urllib.request import urlopen
-import os
-import datetime
-import time
-import sys
+import urllib.request
 import socket
-import requests
 
-
-count = 0
-
-#change to your ESP32-CAM ip
-url="http://192.168.0.15:81/stream"
-CAMERA_BUFFRER_SIZE=4096
-
-stream=urlopen(url)
+stream=urllib.request.urlopen("http://192.168.0.15:81/stream")
 bts=b''
               
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-
-host = "192.168.0.15" #ESP32 IP in local network
-port = 82             #ESP32 Server Port    
- 
-sock.connect((host, port))
+sock.connect(("192.168.0.15", 82))
 
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-#face_cascade = cv.CascadeClassifier('stop_sign.xml')
-#eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
 
 while True: 
-
-    bts+=stream.read(CAMERA_BUFFRER_SIZE)
+    bts+=stream.read(4096)
     jpghead=bts.find(b'\xff\xd8')
     jpgend=bts.find(b'\xff\xd9')
     if jpghead>-1 and jpgend>-1:
@@ -40,12 +21,6 @@ while True:
         buff = np.frombuffer(jpg,dtype=np.uint8)
         if buff.any():
             img=cv.imdecode(buff,cv.IMREAD_UNCHANGED)
-            #v=cv.flip(img,0)
-            #h=cv.flip(img,1)
-            #p=cv.flip(img,-1)        
-            #frame=p
-            #h,w=frame.shape[:2]
-            #img=cv.resize(frame,(800,600))
             gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, 1.3, 5)
             for i in faces:
